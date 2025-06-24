8f11efac0cce7806badf60a4e5760f89d8b8992a
VHE888 <vhe929@gmail.com>
2025-06-24 13:09:44 -0400

    improve the structure of the code 1

--- scripts/ArrowFiles.Generation.by.Archr.R
+++ scripts/ArrowFiles.Generation.by.Archr.R
@@ -1,40 +1,56 @@
 # date: 2025-06-17
 .libPaths("/projectnb/cepinet/libs/R_4.4.0_libs")
 library(ArchR)
-########
-df=read.table('/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/All.ATAC.samp.info.txt',header = T,sep = '\t')
+
+# ==================== Read sample info ====================
+df <- read.table(
+  '/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/All.ATAC.samp.info.txt',
+  header = TRUE,
+  sep = '\t'
+)
+
 table(df$region)
-df=df[df$region!='MB',]
+df <- df[df$region != 'MB', ]
 
 
-inputFiles=list.files(path = '/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/fragments',pattern = 'tsv.gz',full.names = T)
-sNames=c()
-for (f in inputFiles){nm=strsplit(rev(strsplit(f,'/')[[1]])[1],'[.]')[[1]][1];sNames=c(sNames,nm)}
-names(inputFiles)=sNames
+# ==================== Prepare input files ====================
+inputFiles <- list.files(
+  path = '/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/fragments',
+  pattern = 'tsv.gz',
+  full.names = TRUE
+)
 
-ov=intersect(sNames,df$SampID)
-df=df[df$SampID %in% ov,]
-sNames=ov
-inputFiles=inputFiles[ov]
+sNames <- c()
+for (f in inputFiles) {
+  nm <- strsplit(rev(strsplit(f, '/')[[1]])[1], '[.]')[[1]][1]
+  sNames <- c(sNames, nm)
+}
+names(inputFiles) <- sNames
 
-dir.ou.arrow="./arrowFiles/"
-dir.create(dir.ou.arrow, showWarnings=F, recursive=T)
+ov <- intersect(sNames, df$SampID)
+df <- df[df$SampID %in% ov, ]
+sNames <- ov
+inputFiles <- inputFiles[ov]
 
-#####################################################################
-#building arrow files
-# #
-# TileMatrix: binned accessibility (e.g., 500bp bins)
-# GeneScoreMatrix: estimated gene activity scores
-# Cell-level QC metrics: fragment counts, TSS enrichment, etc.
-#####################################################################
-## setup 
+
+# ==================== Create Arrow Files ====================
+dir.ou.arrow <- "./arrowFiles/"
+dir.create(dir.ou.arrow, showWarnings = FALSE, recursive = TRUE)
+
+# Description:
+# - TileMatrix: binned accessibility (e.g., 500bp bins)
+# - GeneScoreMatrix: estimated gene activity scores
+# - Cell-level QC metrics: fragment counts, TSS enrichment, etc.
+
 addArchRThreads(threads = 16)
-addArchRGenome("hg38") 
-####
-ArrowFiles <- createArrowFiles(inputFiles = inputFiles,
-                               sampleNames = sNames,
-                               outputNames= paste0(dir.ou.arrow, sNames),
-                               filterTSS = 6, 
-                               filterFrags = 1000, 
-                               addTileMat = TRUE,
-                               addGeneScoreMat = TRUE)
+addArchRGenome("hg38")
+
+ArrowFiles <- createArrowFiles(
+  inputFiles      = inputFiles,
+  sampleNames     = sNames,
+  outputNames     = paste0(dir.ou.arrow, sNames),
+  filterTSS       = 6, 
+  filterFrags     = 1000, 
+  addTileMat      = TRUE,
+  addGeneScoreMat = TRUE
+)

b429345f89edbdbb9f6f23a9baf4cb3f2f9523cb
VHE888 <vhe929@gmail.com>
2025-06-25 16:48:42 -0400

    change the structure of the code 4.2

--- scripts/4.2.clusterBasedPeakAndDP.R
+++ scripts/4.2.clusterBasedPeakAndDP.R
@@ -1,6 +1,5 @@
-
-#peak calling based on MG cluster
-#and generate pseudobulk signal for differential peak calling for sample groups
+# peak calling based on MG cluster
+# and generate pseudobulk signal for differential peak calling for sample groups
 
 .libPaths("~/cEpiNet/libs/R_4.4.0_libs/")
 library(ArchR)
@@ -11,68 +10,74 @@ library(DESeq2)
 library(GenomicRanges)
 
 addArchRThreads(threads = 16)
-addArchRGenome("hg38") 
+addArchRGenome("hg38")
 
+# file.in.RNA.rds = "../../scRNA/cell-2023-Sun/ROSMAP.Microglia.6regions.seurat.harmony.selected.deidentified.rds"
+file.ou.brain4.rds  <- "brain.microglia.filter/brain4.integRNA.filt.rds"  # pseudo bulk profile based on clusters
+file.in.430.meta     <- "/projectnb/cepinet/data/sc_eQTL/AD430_PFC.eQTLs.Oct2023/1.annotation/patients.metadata.withADdiag.txt"
+file.in.pkSet.rds    <- "./brain.microglia.filter/brain4.integRNA.filt.PeakSet.byClusters.rds"
 
-#file.in.RNA.rds="../../scRNA/cell-2023-Sun/ROSMAP.Microglia.6regions.seurat.harmony.selected.deidentified.rds"
-file.ou.brain4.rds="brain.microglia.filter/brain4.integRNA.filt.rds" #psuedo bulk profile based on clusters and peak calling based on MG clusters
-file.in.430.meta="/projectnb/cepinet/data/sc_eQTL/AD430_PFC.eQTLs.Oct2023/1.annotation/patients.metadata.withADdiag.txt"
-file.in.pkSet.rds="./brain.microglia.filter/brain4.integRNA.filt.PeakSet.byClusters.rds"
+# Load filtered ArchR project
+brain4 <- readRDS(file.ou.brain4.rds)
 
-#
-brain4=readRDS(file.ou.brain4.rds)
-#peakSet.byCluster
+# Update metadata: projid should be character
+cell.meta     <- getCellColData(brain4)
+projid.new    <- as.character(cell.meta$projid)
+names(projid.new) <- rownames(cell.meta)
 
-#update meta
-#projid should be character
-cell.meta <- getCellColData(brain4)
-# Step 2: Modify projid to character
-projid.new <- as.character(cell.meta$projid)
-names(projid.new) <- rownames(cell.meta)  # must be named by cell IDs
 brain4 <- addCellColData(
   ArchRProj = brain4,
-  data = projid.new,
-  cells = names(projid.new),
-  name = "projid",
-  force = TRUE
+  data      = projid.new,
+  cells     = names(projid.new),
+  name      = "projid",
+  force     = TRUE
 )
 
-#getAvailableMatrices(brain4)
-pkSet=readRDS(file.in.pkSet.rds)
-
-brain4=addPeakSet(brain4, peakSet = pkSet, force = TRUE)
+# Load peak set and add peak matrix
+pkSet  <- readRDS(file.in.pkSet.rds)
+brain4 <- addPeakSet(brain4, peakSet = pkSet, force = TRUE)
 brain4 <- addPeakMatrix(brain4)
-#brain4 <- saveArchRProject(ArchRProj = brain4, outputDirectory = "/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/brain.microglia.filter/", load = TRUE)
-#pkSet.test=getPeakSet(brain4)
+
+# Optionally save updated project
+# brain4 <- saveArchRProject(
+#   ArchRProj        = brain4,
+#   outputDirectory  = "/projectnb/cepinet/data/scATAC/Na_Cell_2023_MG/brain.microglia.filter/",
+#   load             = TRUE
+# )
+# pkSet.test <- getPeakSet(brain4)
+
 saveRDS(brain4, file.ou.brain4.rds)
 
+# Generate pseudobulk matrix by sample (projid)
 brain4.pseudobulk.mat <- getGroupSE(
   ArchRProj = brain4,
-  divideN=F,
+  divideN   = FALSE,
   useMatrix = "PeakMatrix",
-  groupBy = "projid"  # or your grouping
+  groupBy   = "projid"  # or your grouping
 )
-saveRDS(brain4.pseudobulk.mat,file='brain.microglia.filter/brain4.integRNA.filt.pseudoBulkBySamp.peakMat.rds')
 
-# #
-# peaks.consensus=readRDS('brain.microglia.filter/getPeakSet.rds')
+saveRDS(
+  brain4.pseudobulk.mat,
+  file = 'brain.microglia.filter/brain4.integRNA.filt.pseudoBulkBySamp.peakMat.rds'
+)
+
+# # Optional: Peak overlap with consensus peak set
+# peaks.consensus <- readRDS('brain.microglia.filter/getPeakSet.rds')
 # peaks.pseudoBulk <- GRanges(
 #   seqnames = brain4.pseudobulk.mat@elementMetadata$seqnames,
 #   ranges = IRanges(
 #     start = brain4.pseudobulk.mat@elementMetadata$start,
-#     end = brain4.pseudobulk.mat@elementMetadata$end
+#     end   = brain4.pseudobulk.mat@elementMetadata$end
 #   )
 # )
 # pks.ovlp <- findOverlaps(peaks.pseudoBulk, peaks.consensus)
 
+# # Read external metadata
+# meta.df <- read.table(file.in.430.meta, sep = "\t", header = TRUE, row.names = 1)
 
-
-# #read meta data
-# meta.df=read.table(file.in.430.meta, sep="\t", header=T, row.names=1)
-
+# # DESeq2 pipeline (commented out)
 # dds <- DESeqDataSet(brain4.pseudobulk.mat, design = ~ 1)  # use real design formula here
 # dds <- estimateSizeFactors(dds)
 # dds <- estimateDispersions(dds)
 # dds <- DESeq(dds)
-
 # res <- results(dds)
